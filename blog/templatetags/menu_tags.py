329e6b3e973f812bef96454d92f4162a00862765
egor_2111 <egor_2111_t@mail.ru>
2022-08-25 15:57:16 +0300

    first commit

---
+++ blog/templatetags/menu_tags.py
@@ -0,0 +1,27 @@
+from django import template
+from blog.models import Categoy
+
+from blog.models import Post
+
+register = template.Library()
+
+
+def get_all_categories():
+    return Categoy.objects.all()
+
+
+@register.simple_tag()
+def get_list_category():
+    return get_all_categories()
+
+
+@register.inclusion_tag('blog/include/tags/top_menu.html')
+def get_categories():
+    category = get_list_category()
+    return {"list_category": category}
+
+
+@register.inclusion_tag('blog/include/tags/recipes_tag.html')
+def get_last_posts():
+    posts = Post.objects.select_related("category").order_by("-id")[:5]
+    return {"list_last_post": posts}
